2cdaa2d6e48015d89f2bbe198b678584c900b355
tinkgu <tlwrite@126.com>
2018-01-11 19:35:17 +0800

    [add] install eslint deps automatically

--- eslint/meta.js
+++ eslint/meta.js
@@ -32,16 +32,46 @@ module.exports = {
             message: 'where is your webpack config file?',
             default: 'webpack.config.js',
         },
-        task: {
-            afterAsk: api => {
-                const renderData = api.metalsmith.metadata()
-                const withReact = ['loose-airbnb-react', 'airbnb']
-                if (withReact.includes(renderData.preset)) {
-                    api.extraRenderData.merge({
-                        withReact: true,
-                    })
-                }
-            },
-        }
     },
+    task: {
+        afterAsk: api => {
+            const renderData = api.metalsmith.metadata()
+            const withReact = ['loose-airbnb-react', 'airbnb']
+            if (withReact.includes(renderData.preset)) {
+                api.extraRenderData.merge({
+                    withReact: true,
+                })
+            }
+        },
+        complete: api => {
+            const renderData = api.metalsmith.metadata()
+            const pkgs = [
+                'babel-core',
+                'babel-eslint',
+                'eslint',
+                'eslint-plugin-import',
+            ]
+            if (renderData.withWebpack) {
+                pkgs.push('eslint-import-resolver-webpack')
+            }
+            const reactPkgs = ['eslint-plugin-jsx-a11y', 'eslint-plugin-react']
+            const preset2pkg = {
+                'loose-airbnb': [
+                    'eslint-config-airbnb-base',
+                    'eslint-config-loose-airbnb',
+                ],
+                'loose-airbnb-react': [
+                    'eslint-config-airbnb',
+                    'eslint-config-loose-airbnb-react',
+                ].concat(reactPkgs),
+                'airbnb-base': [
+                    'eslint-config-airbnb-base',
+                ],
+                'airbnb': [
+                    'eslint-config-airbnb',
+                ].concat(reactPkgs)
+            }
+            api.installDev(preset2pkg[renderData.preset])
+        }
+    }
 }
